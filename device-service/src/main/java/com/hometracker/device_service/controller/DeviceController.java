1b995e70270bb75505a5f866b11f3b7d6577208a
sahilzambre05 <sahildinesh.zambre2023@vitstudent.ac.in>
2026-06-04 15:12:42 +0530

    Added device-service

---
+++ device-service/src/main/java/com/hometracker/device_service/controller/DeviceController.java
@@ -0,0 +1,51 @@
+package com.hometracker.device_service.controller;
+
+import com.hometracker.device_service.dto.DeviceDto;
+import com.hometracker.device_service.service.DeviceService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/device")
+public class DeviceController {
+
+    private DeviceService deviceService;
+
+    public DeviceController(DeviceService deviceService) {
+        this.deviceService = deviceService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DeviceDto> getDeviceById(@PathVariable Long id) {
+        DeviceDto device = deviceService.getDeviceById(id);
+        return ResponseEntity.ok(device);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<DeviceDto> createDevice(@RequestBody DeviceDto deviceDto) {
+        DeviceDto createdDevice = deviceService.createDevice(deviceDto);
+        return ResponseEntity.ok(createdDevice);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DeviceDto> updateDevice(
+            @PathVariable Long id, @RequestBody DeviceDto deviceDto) {
+        DeviceDto updatedDevice = deviceService.updateDevice(id, deviceDto);
+        return ResponseEntity.ok(updatedDevice);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteDevice(@PathVariable Long id) {
+        deviceService.deleteDevice(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<DeviceDto>> getAllDevicesByUserId(
+            @PathVariable Long userId) {
+        List<DeviceDto> devices = deviceService.getAllDevicesByUserId(userId);
+        return ResponseEntity.ok(devices);
+    }
+}
---
+++ device-service/src/main/java/com/hometracker/device_service/exception/DeviceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.hometracker.device_service.exception;
+
+public class DeviceNotFoundException extends RuntimeException {
+    public DeviceNotFoundException(String message) {
+        super(message);
+    }
+}
